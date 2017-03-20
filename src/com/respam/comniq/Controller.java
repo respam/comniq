d1bf6718f02bebcff5233efaa9b695e6bac77ba2
respam <satyaprakash.mahapatra@gmail.com>
2017-03-20 16:31:11 +0530

    added validations for Directory input Field

--- src/com/respam/comniq/Controller.java
+++ src/com/respam/comniq/Controller.java
@@ -24,6 +24,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.DirectoryChooser;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -43,18 +46,50 @@ public class Controller {
     private TextArea textArea;
 
     @FXML
-    private ProgressBar progressBar;
+    private TextFlow textFlow;
+
+    @FXML
+    public ProgressBar progressBar;
 
     String localMovies;
-    @FXML private TextField inputPath;
+    Boolean progBarStatus = false;
+
+    @FXML
+    private TextField inputPath;
 
     @FXML
     protected void handleLocalButtonAction() {
-        progressBar.setProgress(0);
-        parseWorker = createWorker();
-        progressBar.progressProperty().unbind();
-        progressBar.progressProperty().bind(parseWorker.progressProperty());
-        new Thread(parseWorker).start();
+        localMovies = inputPath.getText();
+        File loc = new File(localMovies);
+
+        if (loc.isDirectory() == false) {
+            Text errorMsg = new Text("Not a valid Movies Directory");
+            errorMsg.setFill(Color.RED);
+            textFlow.getChildren().clear();
+            textFlow.getChildren().add(errorMsg);
+        }
+
+        if (progBarStatus == false) {
+            progressBar.setProgress(0);
+            progBarStatus = true;
+        }
+
+        if (progBarStatus == true) {
+            progressBar.relocate(0, 100);
+        }
+
+        if (inputPath.getText().trim().isEmpty()) {
+            Text errorMsg = new Text("Enter a valid Movies Directory");
+            errorMsg.setFill(Color.RED);
+            textFlow.getChildren().clear();
+            textFlow.getChildren().add(errorMsg);
+        } else if (loc.isDirectory() == true && !(inputPath.getText().trim().isEmpty())) {
+            textFlow.getChildren().clear();
+            parseWorker = createWorker();
+            progressBar.progressProperty().unbind();
+            progressBar.progressProperty().bind(parseWorker.progressProperty());
+            new Thread(parseWorker).start();
+        }
     }
 
     public Task createWorker() {
@@ -66,28 +101,32 @@ public class Controller {
                 File loc = new File(localMovies);
                 JSONArray jsonArr = new JSONArray();
 
-                if(loc.isDirectory()) {
-                    String[] folders = loc.list();
-                    for(int i=0; i<folders.length; i++) {
-                        JSONObject jsonObj = new JSONObject();
 
-                        if(folders[i].charAt(0) == '(') {
-                            String[] movieYear = folders[i].split("[()]");
-                            jsonObj.put("movie", movieYear[2].trim());
-                            jsonObj.put("year", movieYear[1]);
-                        }
+                String[] folders = loc.list();
+                for (int i = 0; i < folders.length; i++) {
+                    JSONObject jsonObj = new JSONObject();
 
-                        else {
-                            String[] movieName = folders[i].split("\\(");
-                            String[] movieYear = movieName[1].split("\\)");
-                            jsonObj.put("movie", movieName[0].trim());
-                            jsonObj.put("year", movieYear[0]);
-                        }
-
-                        jsonArr.add(jsonObj);
-                        updateProgress(i, folders.length-1);
+                    if (folders[i].charAt(0) == '(' || folders[i].charAt(0) == '{') {
+                        String[] movieYear = folders[i].split("[({})]");
+                        jsonObj.put("movie", movieYear[2].trim());
+                        jsonObj.put("year", movieYear[1]);
                     }
+                    else if ((folders[i].charAt(folders[i].length() - 1) == ')') || (folders[i].charAt(folders[i].length() - 1) == '}')) {
+                        String[] movieName = folders[i].split("[({]");
+                        String[] movieYear = movieName[1].split("[)}]");
+                        jsonObj.put("movie", movieName[0].trim());
+                        jsonObj.put("year", movieYear[0]);
+                    }
+
+                    else {
+                        jsonObj.put("movie", folders[i].trim());
+                        jsonObj.put("year", null);
+                    }
+
+                    jsonArr.add(jsonObj);
+                    updateProgress(i, folders.length - 1);
                 }
+
                 MovieListParser mp = new MovieListParser();
                 mp.JSONWriter(jsonArr);
                 return true;
@@ -97,7 +136,8 @@ public class Controller {
 
     @FXML
     public void handleOMDBButtonAction() {
-        progressBar.relocate(0,100);
+        progressBar.relocate(0, 100);
+        textFlow.getChildren().clear();
         requestWorker = OMDBworker();
         progressBar.progressProperty().unbind();
         progressBar.progressProperty().bind(requestWorker.progressProperty());
@@ -115,13 +155,13 @@ public class Controller {
                     JSONArray parsedArr = (JSONArray) obj;
 
                     // Loop JSON Array
-                    for(int i=0; i<parsedArr.size(); i++) {
+                    for (int i = 0; i < parsedArr.size(); i++) {
                         JSONObject parsedObj = (JSONObject) parsedArr.get(i);
                         String movieName = (String) parsedObj.get("movie");
                         String movieYear = (String) parsedObj.get("year");
                         System.out.println(movieName + "-" + movieYear);
                         op.requestOMDB(movieName, movieYear);
-                        updateProgress(i, parsedArr.size()-1);
+                        updateProgress(i, parsedArr.size() - 1);
                     }
                     op.movieInfoWriter();
                 } catch (FileNotFoundException e) {
@@ -150,14 +190,14 @@ public class Controller {
             JSONArray parsedArr = (JSONArray) obj;
 
             // Loop JSON Array
-            for(int i=0; i<parsedArr.size(); i++) {
+            for (int i = 0; i < parsedArr.size(); i++) {
                 JSONObject parsedObj = (JSONObject) parsedArr.get(i);
                 textArea.setWrapText(true);
                 textArea.appendText("Title: " + (String) parsedObj.get("Title") + "\n");
                 textArea.appendText("Release Date: " + (String) parsedObj.get("Released") + "\n");
                 textArea.appendText("Genre: " + (String) parsedObj.get("Genre") + "\n");
 
-                textArea.appendText("IMDB Link: " + "http://www.imdb.com/title/" +(String) parsedObj.get("imdbID") + "\n");
+                textArea.appendText("IMDB Link: " + "http://www.imdb.com/title/" + (String) parsedObj.get("imdbID") + "\n");
                 textArea.appendText("IMDB Rating: " + (String) parsedObj.get("imdbRating") + "\n");
                 textArea.appendText("Actors: " + (String) parsedObj.get("Actors") + "\n");
                 textArea.appendText("Plot: " + (String) parsedObj.get("Plot") + "\n");
@@ -177,7 +217,7 @@ public class Controller {
     protected void browseButtonAction() {
         DirectoryChooser dc = new DirectoryChooser();
         File selectedDC = dc.showDialog(null);
-        if(selectedDC != null) {
+        if (selectedDC != null) {
             inputPath.setText(selectedDC.getPath());
         }
     }
